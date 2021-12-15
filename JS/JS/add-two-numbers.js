4e52328f736021c4edbe2f5418907ad671d02d0d
paudeldileep <dileepkpaudel@gmail.com>
2021-12-15 14:00:51 +0545

    JS Basics

---
+++ JS/JS/add-two-numbers.js
@@ -0,0 +1,7 @@
+//ask two numbers using prompt
+var num1=prompt("Enter first number");
+var num2=prompt("Enter second number");
+//add two numbers
+var sum=parseInt(num1)+parseInt(num2);
+//display the sum
+alert("Sum of "+num1+" and "+num2+" is "+sum);
