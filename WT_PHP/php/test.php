8eb7ec70793977d530ceff33b0384440196473b3
paudeldileep <dileepkpaudel@gmail.com>
2022-01-19 11:32:21 +0545

    basics of php

---
+++ WT_PHP/php/test.php
@@ -0,0 +1,42 @@
+<?php
+
+    // $a=2;
+    // $b=3;
+    // $sum=$a+$b;
+    // #echo 'the sum of '.$a.' and '.$b.' is '.$sum;
+    // echo "the sum of $a and $b is $sum";
+
+    // $num=5;
+    // $num2=4.5;
+    // $str='hello';
+    // $bool=true;
+
+    // //printing types
+    // echo 'type of $num is '.gettype($num).'<br>';
+    // echo 'type of $num2 is '.gettype($num2).'<br>';
+    // echo 'type of $str is '.gettype($str).'<br>';
+    // echo 'type of $bool is '.gettype($bool).'<br>';
+
+    // //converting types
+    // $newNum2= (int)$num2;
+    // echo 'type of $newNum2 is '.gettype($newNum2).'<br>';
+
+    // $newStr= (string)$num;
+    // echo 'type of $newStr is '.gettype($newStr).'<br>';
+
+    // //checking using is_int and is_string
+    // if(is_int($newStr)){
+    //     echo 'is an integer<br>';
+    // }
+    // else{
+    //     echo "<h2>is not an integer<h2><br>";
+    // }
+
+    //do while loop to print sum of first 10 numbers
+    //  $i=11;
+    //     $sum=0;
+    //     do{
+    //         $sum=$sum+$i;
+    //         $i++;
+    //     }while($i<=10);
+    //     echo 'sum of first 10 numbers is '.$sum;
